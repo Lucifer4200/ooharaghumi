f4055dabbc8be02e9e22d55bf6c27a4858ceab59
Md.Alamgir <alamgirwordpress1@gmail.com>
2021-01-27 11:08:24 +0600

    scrollTop update

--- assets/js/function.js
+++ assets/js/function.js
@@ -64,6 +64,24 @@
 
   // banner slider activation
 
+  // Scroll To Top 
+  var scrollTop = $(".scrollToTop");
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() < 500) {
+      scrollTop.removeClass("active");
+    } else {
+      scrollTop.addClass("active");
+    }
+  });
+  
+  //Click event to scroll to top
+  $('.scrollToTop').on('click', function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+
 
 
 	// wow animation
